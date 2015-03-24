772da4cdef1b43073a068726e09ce2fbc453d0e9
Brian Hernandez <Brian@Hernandez-cs.com>
2015-03-24 11:45:34 -0400

    User Input included within main.cpp, documentation on future methods.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-main(int argc, char *argv[])
+//calculateIntegral(string userPolynomial)
+//{
+//->breakIntegral();
+//->integrateParts();
+//->combineIntegratedParts();
+//}
+
+int main(int argc, char *argv[])
 {
+string userPolynomial;
+cout << "Please enter the polynomial to be integrated \n";
+cout << "To represent exponents use an arrow, ie for x-squared:x^2";
+cout << "Be sure to put an arithmetic operation between the polynomial terms";
+cin >> userPolynomial;
+return 0;
 }
